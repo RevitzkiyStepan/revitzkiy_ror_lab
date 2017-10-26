2f891241c0a90ca8f817085ec8411946c8813bd6
RevitzkiyStepan <32923187+RevitzkiyStepan@users.noreply.github.com>
2017-10-26 19:45:42 +0300

    so more bug fix

--- schema.rb
+++ schema.rb
@@ -1,3 +1,4 @@
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -16,10 +17,11 @@ ActiveRecord::Schema.define(version: 20171009092312) do
     t.string "firstname"
     t.string "lastname"
     t.string "email"
-    t.string "password"
     t.string "country"
     t.string "city"
-    t.string "house"
+    t.string "street"
+    t.integer "house"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
